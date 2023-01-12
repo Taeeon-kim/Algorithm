96662e39577546851b1610f6239b3c0d83e129b4
taeeon kim <myhom43@gmail.com>
2023-01-12 17:19:31 +0900

    style: 설명 주석 추가

--- Udemy_알고리즘_자료구조/sort/merge_sort.js
+++ Udemy_알고리즘_자료구조/sort/merge_sort.js
@@ -25,9 +25,9 @@ function mergeSort(arr) {
   if (arr.length <= 1) return arr; // base case;
   // recursive case below
   let midpoint = Math.floor(arr.length / 2);
-  let leftArray = mergeSort(arr.slice(0, midpoint));
-  let rightArray = mergeSort(arr.slice(midpoint));
-  return merge(leftArray, rightArray);
+  let leftArray = mergeSort(arr.slice(0, midpoint)); // divide
+  let rightArray = mergeSort(arr.slice(midpoint)); // divide
+  return merge(leftArray, rightArray); // sort and merge
 }
 
 console.log(mergeSort([5, 1, 74, 30, 76, 70]));

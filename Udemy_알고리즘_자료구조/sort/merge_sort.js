d65877b4538e970094c3773a1b5161841fa12ee8
taeeon kim <myhom43@gmail.com>
2023-01-12 15:51:24 +0900

    feat:  병합정렬 이미 정렬된 두 배열 case

---
+++ Udemy_알고리즘_자료구조/sort/merge_sort.js
@@ -0,0 +1,24 @@
+function mergeSort(arr1, arr2) {
+  const results = [];
+  let i = 0;
+  let j = 0;
+  while (i < arr1.length && j < arr2.length) {
+    if (arr1[i] < arr2[j]) {
+      results.push(arr1[i]);
+      i++;
+    } else {
+      results.push(arr2[j]);
+      j++;
+    }
+  }
+  while (i < arr1.length) {
+    results.push(arr1[i]);
+    i++;
+  }
+  while (j < arr2.length) {
+    results.push(arr2[j]);
+    j++;
+  }
+  return results;
+}
+console.log(mergeSort([1, 4, 10], [2, 5, 40, 45, 50]));
